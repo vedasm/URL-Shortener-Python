bf24dd8d93d70840413b4c229c93ab18baf756e1
vedasm <129061546+vedasm@users.noreply.github.com>
2026-06-16 19:23:11 +0530

    Initialized Files

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from app.routers import health
+
+app = FastAPI()
+
+app.include_router(health.router)
+
+@app.get("/")
+def home():
+    return {"message": "Welcome"}
---
+++ app/routers/health.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/health", tags=["Health"])
+
+@router.get("/")
+def health():
+    return {
+        "status": "OK"
+    }
